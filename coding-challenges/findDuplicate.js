da99a4a7743c0eba95ae45811f19dd2e30f93684
analogy47 <actweed47@gmail.com>
2019-02-02 23:53:30 -0500

    update findDuplicate challenge

--- coding-challenges/findDuplicate.js
+++ coding-challenges/findDuplicate.js
@@ -20,16 +20,16 @@ Output: 4 or 1
 // if equal return i
 
 const findDuplicate = array => {
-  let tempArr = [];
-  tempArr = array.map(item => item).sort();
+	let tempArr = [];
+	tempArr = array.map(item => item).sort();
 
-  for (let i = 0; i < tempArr.length; i++) {
-    // console.log(tempArr[i])
-    if (tempArr[i] === tempArr[i + 1]) {
-      return tempArr[i];
-    }
-  }
-  return -1;
+	for (let i = 0; i < tempArr.length; i++) {
+		// console.log(tempArr[i])
+		if (tempArr[i] === tempArr[i + 1]) {
+			return tempArr[i];
+		}
+	}
+	return -1;
 };
 
 const arr1 = [1, 2, 2, 3];
@@ -37,19 +37,36 @@ console.log(findDuplicate(arr1));
 
 // Attempt 2 (using indexOf)
 const findDuplicate = array => {
-  let output = [];
+	let output = [];
 
-  if (array.length <= 1) return -1;
+	if (array.length <= 1) return -1;
 
-  array.forEach(function(element, index) {
-    // Find if there is a duplicate or not
-    if (array.indexOf(element, index + 1) > -1) {
-      // Find if the element is already in the output array or not
-      if (output.indexOf(element) === -1) {
-        output.push(element);
-      }
-    }
-  });
+	array.forEach(function(element, index) {
+		// Find if there is a duplicate or not
+		if (array.indexOf(element, index + 1) > -1) {
+			// Find if the element is already in the output array or not
+			if (output.indexOf(element) === -1) {
+				output.push(element);
+			}
+		}
+	});
 
-  return output;
+	return output;
+};
+
+// Atempt 3
+// add the numbers andd index in array to an object
+// to keep track of them
+// if a key is repeated, return the value (index)
+const findDuplicate = array => {
+	const obj = {};
+
+	for (let i = 0; i < a.length; i++) {
+		if (obj[a[i]] !== undefined) {
+			return a[i];
+		} else {
+			obj[a[i]] = i;
+		}
+	}
+	return -1;
 };
