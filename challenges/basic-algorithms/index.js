30d693338b8fab0d842bd90ed41818fe4f9c7fd7
analogy47 <actweed47@gmail.com>
2019-02-23 23:18:06 -0500

    add count vowels challenge

--- challenges/basic-algorithms/index.js
+++ challenges/basic-algorithms/index.js
@@ -11,6 +11,21 @@ const reverseString = str => {
 		.join('');
 };
 
-console.log(reverseString('algorithms'));
+// Given a string of text containing 0 or more vowels,
+// count the number of vowels that can be found within the text
+const countVowels = str => {
+	const vowels = /[aeiou]/gi;
+	let count = 0;
 
-module.exports = { reverseString };
+	for (let i = 0; i < str.length; i++) {
+		console.log(str[i]);
+		if (str[i].match(vowels)) {
+			count += 1;
+		}
+	}
+	return count;
+};
+
+console.log(countVowels('veritas'));
+
+module.exports = { reverseString, countVowels };
--- challenges/basic-algorithms/spec.js
+++ challenges/basic-algorithms/spec.js
@@ -1,19 +1,38 @@
-const { reverseString } = require("./index");
+const { reverseString, countVowels } = require('./index');
 
-describe("Tests for reverseString", () => {
-	test("reverseString is a function", () => {
-		expect(typeof reverseString).toEqual("function");
+describe('Tests for reverseString', () => {
+	test('reverseString is a function', () => {
+		expect(typeof reverseString).toEqual('function');
 	});
 
-	test("reverses a string of text", () => {
-		expect(reverseString("aeiou")).toEqual("uoiea");
+	test('reverses a string of text', () => {
+		expect(reverseString('aeiou')).toEqual('uoiea');
 	});
 
-	test("reverses a string containing numbers", () => {
-		expect(reverseString("123456789")).toEqual("987654321");
+	test('reverses a string containing numbers', () => {
+		expect(reverseString('123456789')).toEqual('987654321');
 	});
 
-	test("reverses a string containing mixed case characters", () => {
-		expect(reverseString("AsDfGhJkL")).toEqual("LkJhGfDsA");
+	test('reverses a string containing mixed case characters', () => {
+		expect(reverseString('AsDfGhJkL')).toEqual('LkJhGfDsA');
+	});
+});
+
+describe('Test for countVowel', () => {
+	test('countVowel is a fumction', () => {
+		expect(typeof countVowels).toEqual('function');
+	});
+	test('outputs a number', () => {
+		expect(typeof countVowels('test string')).toBe('number');
+	});
+	test('counts the correct number of vowels', () => {
+		expect(countVowels('veritas')).toEqual(3);
+	});
+	test('counts vowels for mixed cases', () => {
+		expect(countVowels('Anything')).toEqual(2);
+	});
+	// neglected
+	test('returns the number of vowels found when string is capitalized', () => {
+		expect(countVowels('AEIOU')).toEqual(5);
 	});
 });
