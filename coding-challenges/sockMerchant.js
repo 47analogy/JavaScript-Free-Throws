1c98155fcc4d845d42b0f2c59388b82b2d97252c
analogy47 <actweed47@gmail.com>
2019-03-22 02:22:19 -0400

    add sockMerchant challenge

---
+++ coding-challenges/sockMerchant.js
@@ -0,0 +1,56 @@
+/*
+John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+Function Description
+
+Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+
+sockMerchant has the following parameter(s):
+
+n: the number of socks in the pile
+ar: the colors of each sock
+Input Format
+
+The first line contains an integer , the number of socks represented in . 
+The second line contains  space-separated integers describing the colors  of the socks in the pile.
+
+Constraints
+
+ where 
+Output Format
+
+Return the total number of matching pairs of socks that John can sell.
+
+Sample Input
+
+9
+10 20 20 10 10 30 50 10 20
+Sample Output
+
+3
+*/
+
+function sockMerchant(n, ar) {
+	const socks = {};
+	let count = 0;
+
+	// create sock object
+	for (let i = 0; i < n; i++) {
+		if (!socks[ar[i]]) {
+			socks[ar[i]] = 0;
+		}
+		socks[ar[i]]++;
+	}
+	// creat array of matching socks
+	let socksMatching = Object.values(socks);
+
+	// get all pairs of socks
+	socksMatching.forEach(pair => {
+		if (pair % 2 === 0 || pair % 2 === 1) {
+			count = count + Math.floor(pair / 2);
+		}
+	});
+	return count;
+}
