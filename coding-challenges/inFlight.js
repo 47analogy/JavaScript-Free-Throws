182deab02d76ebb1b89e28cfeea5250efec85739
analogy47 <actweed47@gmail.com>
2019-04-03 13:45:09 -0400

    add better solution for inflight coding challenge

--- coding-challenges/inFlight.js
+++ coding-challenges/inFlight.js
@@ -23,3 +23,35 @@ function inflight(flightLength, movieLength) {
 	}
 	return false;
 }
+
+console.log(inflight(10, [4, 3, 2, 5, 7]));
+
+// Better version
+function inflight(flightLength, movieLength) {
+	let firstMovie = 0;
+	let secondMovie = 0;
+
+	// create set for movies
+	const movies = new Set();
+
+	// loop through moviesLengths
+	for (let i = 0; i < movieLength.length; i++) {
+		firstMovie = movieLength[i];
+
+		// check if movies set has 2 movies equal to flightLength
+
+		// ie for firstMovie = 4, we want secondMovie = 6
+		secondMovie = flightLength - firstMovie;
+		// ie look for secondMovie = 6 in the movies set
+		if (movies.has(secondMovie)) {
+			return true;
+		}
+		// not equal to flight length, so add the movie to set
+		// and keep looking
+		movies.add(firstMovie);
+	}
+	// none of the movies equals flightLength
+	return false;
+}
+
+console.log(inflight(10, [4, 9, 2, 9, 7]));
