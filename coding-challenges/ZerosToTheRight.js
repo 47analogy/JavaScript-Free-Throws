86ad81db4c1bbb5156560ee9fe5007033f54f3dd
analogy47 <actweed47@gmail.com>
2019-07-15 20:19:44 -0400

    new solution moveZeros

--- coding-challenges/ZerosToTheRight.js
+++ coding-challenges/ZerosToTheRight.js
@@ -16,32 +16,32 @@ Expected output array state: [4, 2, 1, 5]
 
 // first solution
 const zerosToRight = arr => {
-  // counter variable
-  let counter = 0;
-  const length = arr.length; // make sure every element is included in loop operations
+	// counter variable
+	let counter = 0;
+	const length = arr.length; // make sure every element is included in loop operations
 
-  // loop thru arr
-  for (let i = 0; i < length; i++) {
-    // if element is 0
-    if (arr[i] === 0) {
-      // remove it
-      arr.splice(i, 1);
-      // add 0 to end of arr
-      arr.push(0);
-    }
-  }
+	// loop thru arr
+	for (let i = 0; i < length; i++) {
+		// if element is 0
+		if (arr[i] === 0) {
+			// remove it
+			arr.splice(i, 1);
+			// add 0 to end of arr
+			arr.push(0);
+		}
+	}
 
-  // loop thru arr
-  for (let i = 0; i < length; i++) {
-    // if element is not 0
-    if (arr[i] !== 0) {
-      // increase counter
-      counter += 1;
-    }
-  }
-  console.log(arr); // confirm that array output is good
-  // return counter
-  return counter;
+	// loop thru arr
+	for (let i = 0; i < length; i++) {
+		// if element is not 0
+		if (arr[i] !== 0) {
+			// increase counter
+			counter += 1;
+		}
+	}
+	console.log(arr); // confirm that array output is good
+	// return counter
+	return counter;
 };
 
 console.log(zerosToRight([0, 3, 1, 0, -2]));
@@ -49,35 +49,50 @@ console.log(zerosToRight([4, 2, 1, 5]));
 
 // second solution - works for any number (not just zero)
 const anyNumToRight = (arr, num) => {
-  // counter variable
-  let counter = 0;
-  const length = arr.length; // make sure every element is included in loop operations
+	// counter variable
+	let counter = 0;
+	const length = arr.length; // make sure every element is included in loop operations
 
-  // loop thru arr
-  for (let i = 0; i < length; i++) {
-    // if element is num
-    if (arr[i] === num) {
-      // remove it
-      arr.splice(i, 1);
-      // add num to end of arr
-      arr.push(num);
-    }
-  }
+	// loop thru arr
+	for (let i = 0; i < length; i++) {
+		// if element is num
+		if (arr[i] === num) {
+			// remove it
+			arr.splice(i, 1);
+			// add num to end of arr
+			arr.push(num);
+		}
+	}
 
-  // loop thru arr
-  for (let i = 0; i < length; i++) {
-    // if element is not 0
-    if (arr[i] !== num) {
-      // increase counter
-      counter += 1;
-    }
-  }
-  console.log(arr); // confirm that array output is good
-  // return counter
-  return counter;
+	// loop thru arr
+	for (let i = 0; i < length; i++) {
+		// if element is not 0
+		if (arr[i] !== num) {
+			// increase counter
+			counter += 1;
+		}
+	}
+	console.log(arr); // confirm that array output is good
+	// return counter
+	return counter;
 };
 
 console.log(anyNumToRight([0, 3, 1, 0, -2, 3], 3));
 console.log(anyNumToRight([2, 4, 2, 1, 5], 2));
 
-// TODO: try to reduce to 1 loop (i.e. get count while moving items)
+// third solution - uses 1 loop
+const moveZeros = arr => {
+	// loop arr backwards
+	for (let i = arr.length - 1; i >= 0; i--) {
+		// look for 0
+		if (arr[i] === 0) {
+			// remove it and place at end of arr
+			arr.splice(i, 1);
+			arr.push(0);
+		}
+	}
+	return arr;
+};
+
+console.log(moveZeros([0, 3, 1, 0, -2]));
+console.log(moveZeros([4, 2, 0, 1, 5]));
