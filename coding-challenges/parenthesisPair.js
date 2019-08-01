273615f37fe9f18100720f68ffcda69e166505a2
analogy47 <actweed47@gmail.com>
2019-07-31 23:10:41 -0400

    update parenthesisPair

--- coding-challenges/parenthesisPair.js
+++ coding-challenges/parenthesisPair.js
@@ -10,29 +10,29 @@ parenthesisPair(str, 10) => 79
 */
 
 // use a 'stack'
-// const parenthesisPair = (str, pos) => {
-// 	const opening = '(';
-// 	const closing = ')';
+const parenthesisPair = (str, pos) => {
+	const opening = '(';
+	const closing = ')';
 
-// 	// counter to keep track of matching pair
-// 	let count = 0;
+	// counter to keep track of matching pair
+	let count = 0;
 
-// 	// loop through string from position pos
-// 	for (let i = pos + 1; i < str.length; i++) {
-// 		// if '(' then increase the count
-// 		if (str[i] === opening) {
-// 			count = count + 1;
-// 			// if ')' then check the count
-// 		} else if (str[i] === closing) {
-// 			// if count is 0 it's a match and return index
-// 			if (count === 0) {
-// 				return i;
-// 			}
-// 			// other decrease count and continue
-// 			count = count - 1;
-// 		}
-// 	}
-// };
+	// loop through string from position pos
+	for (let i = pos + 1; i < str.length; i++) {
+		// if '(' then increase the count
+		if (str[i] === opening) {
+			count = count + 1;
+			// if ')' then check the count
+		} else if (str[i] === closing) {
+			// if count is 0 it's a match and return index
+			if (count === 0) {
+				return i;
+			}
+			// other decrease count and continue
+			count = count - 1;
+		}
+	}
+};
 
 // EXPLICIT
 // loop thru string
@@ -40,7 +40,6 @@ parenthesisPair(str, 10) => 79
 //      if closing => check stack capacity
 //          if stack empty => return index
 //          else remove from stack
-
 const parenthesisPair = (str, pos) => {
 	const stack = [];
 	const opening = '(';
