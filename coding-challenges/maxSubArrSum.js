2d8223821b4371f43b00c9df728ae5c3fe7fd3b5
analogy47 <actweed47@gmail.com>
2019-08-17 01:25:55 -0400

    correct maxSubArrSum

--- coding-challenges/maxSubArrSum.js
+++ coding-challenges/maxSubArrSum.js
@@ -9,28 +9,27 @@ maxSubArrSum([], 4) // null
 */
 
 const maxSubArrSum = (arr, n) => {
-    let tempMax = 0;
-    let maxSum = -Infinity; // account for negative numbers
+  let tempMax = 0
+  let maxSum = -Infinity // account for negative numbers
 
-    if (arr.length < n) return null;
+  if (arr.length < n) return null
 
-    // loop over first n numbers and store sum as a tempMax
-    for (let i = 0; i < n; i++) {
-        let num = arr[i];
-        tempMax += num;
-    }
-   
-    // loop over array starting at index = num
-    for (let i = n; i < arr.length; i++) {
-        let prevItem =  arr[i - n]
-        let nextItem = arr[i]
-        
-        // store maximum sum
-        maxSum = Math.max(tempMax, maxSum)
-        //subtract first item and add one item going to the right
-        tempMax = tempMax - prevItem + nextItem;
-    }
-    return maxSum;
-};
+  // loop over first n numbers and store sum as a tempMax
+  for (let i = 0; i < n; i++) {
+    let num = arr[i]
+    tempMax += num
+  }
 
-console.log(maxSubArrSum([1, 2, 5, 2, 8, 1, 5], 2));
+  // loop over array starting at index = num
+  for (let i = n; i < arr.length; i++) {
+    let prevItem = arr[i - n]
+    let nextItem = arr[i]
+
+    // subtract first item and add one item going to the right
+    tempMax = tempMax - prevItem + nextItem
+
+    // store maximum sum
+    maxSum = Math.max(tempMax, maxSum)
+  }
+  return maxSum
+}
