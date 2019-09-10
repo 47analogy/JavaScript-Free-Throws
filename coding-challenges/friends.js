0b155aad61d8fe70b1ea797af47b46af806c9ecf
analogy47 <actweed47@gmail.com>
2019-09-09 22:10:25 -0400

    add friends

---
+++ coding-challenges/friends.js
@@ -0,0 +1,25 @@
+/*
+Write a program that filters a list of strings 
+and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure
+ that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] => ["Ryan", "Mark"]
+*/
+
+function friend(friends) {
+  const myFriends = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    let name = friends[i];
+    if (name.length === 4 && isNaN(name)) {
+      myFriends.push(name);
+    }
+  }
+  return myFriends;
+}
