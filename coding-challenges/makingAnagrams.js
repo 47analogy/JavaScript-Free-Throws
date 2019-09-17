c3d84dd1cad6e6ee107aba5573bf57067b85219f
analogy47 <actweed47@gmail.com>
2019-09-16 21:09:46 -0400

    add makingAnagrams

--- coding-challenges/makingAnagrams.js
+++ coding-challenges/makingAnagrams.js
@@ -15,47 +15,47 @@ Sample Output:
 */
 
 function makingAnagrams(s1, s2) {
- const wordOne = {};
- const wordTwo = {};
+  const wordOne = {};
+  const wordTwo = {};
 
- for (let char of s1) {
-  if (!wordOne[char]) {
-   wordOne[char] = 0;
+  for (let char of s1) {
+    if (!wordOne[char]) {
+      wordOne[char] = 0;
+    }
+    wordOne[char]++;
   }
-  wordOne[char]++;
- }
 
- for (let char of s2) {
-  if (!wordTwo[char]) {
-   wordTwo[char] = 0;
+  for (let char of s2) {
+    if (!wordTwo[char]) {
+      wordTwo[char] = 0;
+    }
+    wordTwo[char]++;
   }
-  wordTwo[char]++;
- }
 
- // array to hold deletions
- const sum = [];
+  // array to hold deletions
+  const sum = [];
 
- // find difference in common properties
- for (let key in wordOne) {
-  // if both objects have the same property, get the difference in frequency
-  if (wordOne.hasOwnProperty(key) && wordTwo.hasOwnProperty(key)) {
-   sum.push(Math.abs(wordOne[key] - wordTwo[key]));
-   // get unique properties in wordOne
-  } else {
-   sum.push(wordOne[key]);
+  // find difference in common properties
+  for (let key in wordOne) {
+    // if both objects have the same property, get the difference in frequency
+    if (wordOne.hasOwnProperty(key) && wordTwo.hasOwnProperty(key)) {
+      sum.push(Math.abs(wordOne[key] - wordTwo[key]));
+      // get unique properties in wordOne
+    } else {
+      sum.push(wordOne[key]);
+    }
   }
- }
 
- // get unique properties in wordTwo
- for (let key in wordTwo) {
-  if (wordTwo.hasOwnProperty(key) !== wordOne.hasOwnProperty(key)) {
-   sum.push(wordTwo[key]);
+  // get unique properties in wordTwo
+  for (let key in wordTwo) {
+    if (wordTwo.hasOwnProperty(key) !== wordOne.hasOwnProperty(key)) {
+      sum.push(wordTwo[key]);
+    }
   }
- }
 
- const deletions = sum.reduce((acc, cum) => {
-  return acc + cum;
- });
+  const deletions = sum.reduce((acc, cum) => {
+    return acc + cum;
+  });
 
- return deletions;
+  return deletions;
 }
