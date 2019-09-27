5d8d97bb07eaf6faa66b7a6ae5e41cf47e150387
analogy47 <actweed47@gmail.com>
2019-09-27 00:31:13 -0400

    add recursive factorial

--- coding-challenges/recursion.js
+++ coding-challenges/recursion.js
@@ -1,12 +1,27 @@
 /*
-Write function that accepts a base and an exponent. The function 
+Write function that accepts a base and an exponent. The function
 should return the power of the base to the exponent.
 */
 
-function power(base, exp) {
+function power (base, exp) {
   if (exp === 0) {
-    return 1;
+    return 1
   } else {
-    return base * power(base, exp - 1);
+    return base * power(base, exp - 1)
+  }
+}
+
+/*
+Write function factorial that accepts a number and return the factorial
+of that number.
+i.e. factorial(4) = 4 * 3 * 2 * 1  = 24
+Note: Factorial (0) is 1.
+*/
+
+function factorial (n) {
+  if (n === 0) {
+    return 1
+  } else {
+    return n * factorial(n - 1)
   }
 }
