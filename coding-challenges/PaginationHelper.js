88560f55f3692d950d1fdcf78370ac786138cf42
analogy47 <actweed47@gmail.com>
2019-12-24 21:45:36 -0500

    add itemCount and pageCount methods

---
+++ coding-challenges/PaginationHelper.js
@@ -0,0 +1,31 @@
+// TODO: complete this object/class
+
+// The constructor takes in an array of items and a integer indicating how many
+// items fit within a single page
+function PaginationHelper(collection, itemsPerPage){
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+}
+
+// returns the number of items within the entire collection
+PaginationHelper.prototype.itemCount = function() {
+    return this.collection.length;
+}
+
+// returns the number of pages
+PaginationHelper.prototype.pageCount = function() {
+    return Math.ceil(this.collection.length / this.itemsPerPage);
+}
+
+// returns the number of items on the current page. page_index is zero based.
+// this method should return -1 for pageIndex values that are out of range
+PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+
+}
+
+// determines what page an item is on. Zero based indexes
+// this method should return -1 for itemIndex values that are out of range
+PaginationHelper.prototype.pageIndex = function(itemIndex) {
+
+}
+
