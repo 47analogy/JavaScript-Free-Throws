ebb23e722105be237b7160250c884f93de0be366
analogy47 <actweed47@gmail.com>
2020-01-09 00:34:28 -0500

    refactor - no division

--- coding-challenges/productOfAllNums.js
+++ coding-challenges/productOfAllNums.js
@@ -15,19 +15,44 @@
 
 // solush1 - w/ division
 const productOfAllNums = arr => {
-	const prodArr = [];
-	let product;
+  const prodArr = [];
+  let product;
 
-	// get product of arr
-	product = arr.reduce((total, num) => {
-		return total * num;
-	});
+  // get product of arr
+  product = arr.reduce((total, num) => {
+    return total * num;
+  });
 
-	for (let i = 0; i < arr.length; i++) {
-		prodArr.push(product / arr[i]);
-	}
+  // divide product by each num in array
+  for (let i = 0; i < arr.length; i++) {
+    prodArr.push(product / arr[i]);
+  }
 
-	return prodArr;
+  return prodArr;
+};
+
+// solush2 - no division
+const productOfAllNums = arr => {
+  const prodArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let removed = arr[i];
+    // use multiplicative identity property
+    arr[i] = 1;
+    prodArr.push(getProduct(arr));
+    // put number back
+    arr[i] = removed;
+  }
+
+  // can't use arrow syntax or will be undefined in for-loop
+  function getProduct(modifiedArr) {
+    const product = modifiedArr.reduce((prod, num) => {
+      return prod * num;
+    });
+    return product;
+  }
+
+  return prodArr;
 };
 
 productOfAllNums([1, 7, 3, 4]); // [84, 12, 28, 21]
