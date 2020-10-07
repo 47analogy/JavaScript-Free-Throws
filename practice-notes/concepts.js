483dca8e8fbf3ae9c74cde21bef284f86eb2045d
analogy47 <actweed47@gmail.com>
2020-10-06 23:23:53 -0400

    add notes about promises

--- practice-notes/concepts.js
+++ practice-notes/concepts.js
@@ -30,3 +30,47 @@ console.log(colorNumbers);
 const numberColors = [4, 5, 6, ...colors];
 
 console.log(numberColors);
+
+// Promise object
+// pending, fulfilled, rejected
+function promiseTimeout(ms) {
+  return new Promise((resolve, reject) => {
+    // function that takes some time to execute
+    setTimeout(resolve, ms);
+  });
+}
+
+promiseTimeout(2000)
+  .then(() => {
+    console.log('Done!!');
+    return promiseTimeout(1000);
+  })
+  .then(() => {
+    console.log('Also done!!');
+    return Promise.resolve(42); // helper
+  })
+  .then((result) => {
+    console.log(result); // 42
+  })
+  .catch(() => {
+    console.log('Error!');
+  });
+
+// async / await
+async function longRunningOperation() {
+  // logic
+  return 42;
+}
+
+async function run() {
+  // logic
+  console.log('Start!!');
+  await promiseTimeout(2000); // takes some time to execute / waits 2 secs
+
+  const response = await longRunningOperation(); // waits for promiseTimeOut to complete b/f running
+  console.log(response);
+
+  console.log('Stop!!');
+}
+
+run();
