cac267e39662401d838cab29bad078cb57cd4fb3
analogy47 <actweed47@gmail.com>
2020-10-10 20:21:37 -0400

    add more notes about spread operator

--- practice-notes/concepts.js
+++ practice-notes/concepts.js
@@ -11,25 +11,40 @@ const { name, breed } = dog;
 
 console.log(`The dog\'s name is ${name} and he\'s a ${breed}`);
 
-// Spread Operator
+/* 
+Spread Operator allows iterable to spread/expand inside the receiver
+i.e. copy all items in one or more arrays/objects to another array/object
+
+**Useful for making copies, without reference to the original 
+and overwriting object properties 
+*/
 
 // useful for for passing an array items as separate arguments to a function
 const displayNames = (name1, name2, name3) => {
-  console.log(`${name1}, ${name2}, ${name3}`);
+  return `${name1}, ${name2}, ${name3}`;
 };
 
-displayNames(...['Moe', 'Larry', 'Curly']);
+console.log(displayNames(...['Moe', 'Larry', 'Curly']));
 
 // useful for combining arrays
 const numbers = [1, 2, 3];
 const colors = ['red', 'blue', 'green'];
 const colorNumbers = [...colors, ...numbers];
+const favColor = 'purple';
+
+const allColors = [...colors, favColor];
+console.log(allColors);
 
 console.log(colorNumbers);
 
 const numberColors = [4, 5, 6, ...colors];
 
 console.log(numberColors);
+
+// can also be used similar to .split on a string
+const team = 'Yankees';
+const teamLetters = [...team];
+console.log([...teamLetters]);
 
 // Promise object
 // pending, fulfilled, rejected
@@ -74,3 +89,21 @@ async function run() {
 }
 
 run();
+
+///
+const x = { testKeyId: 'test' };
+
+console.log([x]);
+
+const peopleObj = {
+  jim: {
+    name: 'jim',
+    age: 20,
+  },
+  tim: {
+    name: 'tim',
+    age: 22,
+  },
+};
+const peopleArray = Object.values(peopleObj);
+console.log(peopleArray);
